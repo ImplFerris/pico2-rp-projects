d4448e11c0c9297a51a1ed71d7c691618745b75a
ImplFerris <implferris@proton.me>
2025-12-06 21:19:18 +0530

    updated ultrasonic module

--- ultrasonic/src/main.rs
+++ ultrasonic/src/main.rs
@@ -1,24 +1,40 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::digital::{InputPin, OutputPin};
-use panic_halt as _;
-
+use embedded_hal::delay::DelayNs;
+use hal::block::ImageDef;
 use rp235x_hal as hal;
 
-use embedded_hal::delay::DelayNs;
+//Panic Handler
+use panic_probe as _;
+// Defmt Logging
+use defmt_rtt as _;
+
+// For GPIO
+use embedded_hal::digital::{InputPin, OutputPin};
+
+// For PWM
 use embedded_hal::pwm::SetDutyCycle;
 
-#[link_section = ".start_block"]
+/// Tell the Boot ROM about our application
+#[unsafe(link_section = ".start_block")]
 #[used]
-pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
-
+pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
+/// Adjust if your board has a different frequency
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 #[hal::entry]
 fn main() -> ! {
+    // Grab our singleton objects
     let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         XTAL_FREQ_HZ,
         pac.XOSC,
@@ -31,8 +47,10 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
 
+    // Set the pins up according to their function on this particular board
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -85,14 +103,16 @@ fn main() -> ! {
     }
 }
 
-/// Program metadata for `picotool info`
-#[link_section = ".bi_entries"]
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[unsafe(link_section = ".bi_entries")]
 #[used]
 pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
     hal::binary_info::rp_cargo_bin_name!(),
     hal::binary_info::rp_cargo_version!(),
-    hal::binary_info::rp_program_description!(c"PWM Blinky Example"),
+    hal::binary_info::rp_program_description!(c"your program description"),
     hal::binary_info::rp_cargo_homepage_url!(),
     hal::binary_info::rp_program_build_attribute!(),
 ];
 
+// End of file

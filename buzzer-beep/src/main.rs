3136301c24d722d53f90318a4c5938d1c9b34153
ImplFerris <implferris@proton.me>
2025-12-24 20:57:22 +0530

    buzzer beep

--- buzzer-beep/src/main.rs
+++ buzzer-beep/src/main.rs
@@ -2,26 +2,41 @@
 #![no_main]
 
 use embedded_hal::delay::DelayNs;
-use embedded_hal::pwm::SetDutyCycle;
 use hal::block::ImageDef;
-use panic_halt as _;
 use rp235x_hal as hal;
 
+// For PWM
+use embedded_hal::pwm::SetDutyCycle;
+
+//Panic Handler
+use panic_probe as _;
+// Defmt Logging
+use defmt_rtt as _;
+
 /// Tell the Boot ROM about our application
-#[link_section = ".start_block"]
+#[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
-
 /// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
 /// Adjust if your board has a different frequency
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 const fn get_top(freq: f64, div_int: u8) -> u16 {
+    assert!(div_int != 0, "Divider must not be 0");
+
     let result = 150_000_000. / (freq * div_int as f64);
+
+    assert!(result >= 1.0, "Frequency too high");
+    assert!(
+        result <= 65535.0,
+        "Frequency too low: TOP exceeds 65534 max"
+    );
+
     result as u16 - 1
 }
 
 const PWM_DIV_INT: u8 = 64;
+const PWM_TOP: u16 = get_top(440., PWM_DIV_INT);
 
 #[hal::entry]
 fn main() -> ! {
@@ -59,18 +74,16 @@ fn main() -> ! {
 
     let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
 
-    // Init PWMs
     let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
-
     let pwm = &mut pwm_slices.pwm7;
-    pwm.enable();
+
+    pwm.set_top(PWM_TOP);
     pwm.set_div_int(PWM_DIV_INT);
+    pwm.set_div_frac(0);
+    pwm.enable();
+
     pwm.channel_b.output_to(pins.gpio15);
 
-    // Set a frequency (e.g., 440 Hz for an A4 note)
-    let top = get_top(440., PWM_DIV_INT);
-
-    pwm.set_top(top);
     loop {
         pwm.channel_b.set_duty_cycle_percent(50).unwrap();
         timer.delay_ms(500);
@@ -81,12 +94,12 @@ fn main() -> ! {
 
 // Program metadata for `picotool info`.
 // This isn't needed, but it's recomended to have these minimal entries.
-#[link_section = ".bi_entries"]
+#[unsafe(link_section = ".bi_entries")]
 #[used]
 pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
     hal::binary_info::rp_cargo_bin_name!(),
     hal::binary_info::rp_cargo_version!(),
-    hal::binary_info::rp_program_description!(c"Buzzer"),
+    hal::binary_info::rp_program_description!(c"your program description"),
     hal::binary_info::rp_cargo_homepage_url!(),
     hal::binary_info::rp_program_build_attribute!(),
 ];

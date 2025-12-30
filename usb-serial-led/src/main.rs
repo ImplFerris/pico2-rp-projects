ff2c0a7790d31c21161d3879f1c0d4576ea80036
ImplFerris <implferris@proton.me>
2025-12-30 07:52:05 +0530

    updated usb serial

--- usb-serial-led/src/main.rs
+++ usb-serial-led/src/main.rs
@@ -1,25 +1,40 @@
 #![no_std]
 #![no_main]
 
-use embedded_hal::digital::OutputPin;
 use hal::block::ImageDef;
-use panic_halt as _;
 use rp235x_hal as hal;
 
+//Panic Handler
+use panic_probe as _;
+
+// USB Device support
 use usb_device::{class_prelude::*, prelude::*};
+
+// USB Communications Class Device support
 use usbd_serial::SerialPort;
 
-#[link_section = ".start_block"]
+// For the LED
+use embedded_hal::digital::OutputPin;
+
+/// Tell the Boot ROM about our application
+#[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
-
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
+/// Adjust if your board has a different frequency
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
 #[hal::entry]
 fn main() -> ! {
+    // Grab our singleton objects
     let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         XTAL_FREQ_HZ,
         pac.XOSC,
@@ -31,15 +46,20 @@ fn main() -> ! {
     )
     .ok()
     .unwrap();
-    let timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
 
+    // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins up according to their function on this particular board
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+
+    let timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
     let mut led = pins.gpio25.into_push_pull_output();
 
     let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
@@ -62,23 +82,22 @@ fn main() -> ! {
         .build();
 
     let mut said_hello = false;
+
     loop {
-        // Send data to the PC
         if !said_hello && timer.get_counter().ticks() >= 2_000_000 {
             said_hello = true;
             // Writes bytes from `data` into the port and returns the number of bytes written.
             let _ = serial.write(b"Hello, Rust!\r\n");
         }
 
-        // Read data from PC
         if usb_dev.poll(&mut [&mut serial]) {
             let mut buf = [0u8; 64];
             if let Ok(count) = serial.read(&mut buf) {
                 for &byte in &buf[..count] {
                     if byte == b'r' {
-                        led.set_high().unwrap();
+                        led.set_high().expect("unable to turn on LED");
                     } else {
-                        led.set_low().unwrap();
+                        led.set_low().expect("unable to turn off LED");
                     }
                 }
             }
@@ -86,12 +105,16 @@ fn main() -> ! {
     }
 }
 
-#[link_section = ".bi_entries"]
+// Program metadata for `picotool info`.
+// This isn't needed, but it's recomended to have these minimal entries.
+#[unsafe(link_section = ".bi_entries")]
 #[used]
 pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
     hal::binary_info::rp_cargo_bin_name!(),
     hal::binary_info::rp_cargo_version!(),
-    hal::binary_info::rp_program_description!(c"USB Fun"),
+    hal::binary_info::rp_program_description!(c"your program description"),
     hal::binary_info::rp_cargo_homepage_url!(),
     hal::binary_info::rp_program_build_attribute!(),
 ];
+
+// End of file
